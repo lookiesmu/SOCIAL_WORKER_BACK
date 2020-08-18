5e99c5af1bd6c6d870e79260d6c1773d8a71d7a3
hj <dbghwns66@daum.net>
2020-08-18 23:01:47 +0900

    init

---
+++ src/test/java/ac/kr/smu/lookie/socialworker/SocialworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.smu.lookie.socialworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialworkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
