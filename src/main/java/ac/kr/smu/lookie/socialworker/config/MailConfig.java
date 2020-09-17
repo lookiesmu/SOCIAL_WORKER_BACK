667aacf621d6674be7d3eaf3f64416070c4940f8
dowon <devdw98@gmail.com>
2020-09-18 01:20:36 +0900

    user 기능 - 회원가입, 중복 아이디/닉네임 검사, 비밀번호 수정, 아이디찾기, 로그인

--- src/main/java/ac/kr/smu/lookie/socialworker/config/MailConfig.java
+++ src/main/java/ac/kr/smu/lookie/socialworker/config/MailConfig.java
@@ -1,11 +1,19 @@
 package ac.kr.smu.lookie.socialworker.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties
+//@ConfigurationProperties
 public class MailConfig {
+   @Bean
+    public JavaMailSenderImpl mailSender(){
+       JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
+       javaMailSender.setProtocol("smtp");
+       javaMailSender.setHost("127.0.0.1");
+       javaMailSender.setPort(25);
+       return javaMailSender;
+   }
 }
--- src/main/java/ac/kr/smu/lookie/socialworker/domain/User.java
+++ src/main/java/ac/kr/smu/lookie/socialworker/domain/User.java
@@ -1,26 +1,26 @@
 package ac.kr.smu.lookie.socialworker.domain;
 
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Stream;
 
-@Entity
-@Table
-@NoArgsConstructor (access = AccessLevel.PROTECTED)
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
+@Entity
 public class User implements UserDetails {
 
     @Id
-    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -31,56 +31,60 @@ public class User implements UserDetails {
     private String nickname; //닉네임
 
     @Column
-    private String email; //이메일
-
-    @Column
     private String username; //아이디
 
+    @Column(length = 100, nullable = false, unique = true)
+    private String email; //이메일
+
+    @Column(length = 300, nullable = false)
+//    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
     @Column
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private String password;  //비밀번호
+    private String role; //권한 관련 찾아보기
 
     @Column
     private String location; //지역
 
     @Column
 //    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private int anonymity; //익명여부
+    private boolean anonymity; //익명여부
 
     @Column
     private int point; //포인트
 
-    @Column
-//    private UserRole role; //권한
-    private Collection<Role> role; //더 찾아보기
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() { //계정이 갖고있는 권한 목록 리턴
-//        List<UserRole> roles = new ArrayList<>();
-        return role;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return this.roles.stream()
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
+        List<GrantedAuthority> auth = new ArrayList<>();
+        auth.add(new SimpleGrantedAuthority(this.getRole()));
+        return auth;
     }
 
     @Override
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    public boolean isAccountNonExpired() { //계정 만료되어있는지 확인
-        return true; //만료 안됨
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
     @Override
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    public boolean isAccountNonLocked() { //계정 잠겨있는지 학인
-        return true; //잠기지 않음
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
     @Override
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    public boolean isCredentialsNonExpired() { //비밀번호 만료 여부 확인
-        return true; //만료 안됨
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
     @Override
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    public boolean isEnabled() { //계정 활성화 상태인지 확인
-        return true; //활성화
+    public boolean isEnabled() {
+        return true;
     }
 }
